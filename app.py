8b035794b954ebdd13d95e1909d5456e61fa0fbf
KiranYadav-007 <90244854+KiranYadav-007@users.noreply.github.com>
2022-05-22 12:42:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,5 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')#home route
 def index():
-	return 'Hello World with Flask'
+	return 'Hello World with Flask - by Kiran Yadav'
 #app.run()
